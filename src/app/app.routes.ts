68d3a53f6bc5e64bacddb32b1ddd889ac4c7a965
Murat Karabiyik <muratkarabiyik@hotmail.co.uk>
2026-02-18 10:19:12 +0000

    CSS styling improved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { RegisterComponent } from './component/register/register.component';
 import { HomeComponent } from './component/home/home.component';
 import { CartComponent } from './component/cart/cart.component';
 import { AboutComponent } from './component/about/about.component';
-import { authGuard } from './guards/auth.guard';
+
+import { ContactComponent } from './component/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: 'products', loadComponent: () => import('./component/products/products.component').then(m => m.ProductsComponent) },
-  { path: 'cart', component: CartComponent, canActivate: [authGuard] },
-  { path: 'about', component: AboutComponent, canActivate: [authGuard] },
+  { path: 'cart', component: CartComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
